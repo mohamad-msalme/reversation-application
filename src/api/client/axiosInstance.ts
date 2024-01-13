73307625ad02cc06138466112a76abaa45a930aa
Mohamad Masalmeh <mohamad.msalme77@gmail.com>
2024-01-13 19:05:46 +0300

    fix: remove proxy frm package json and replace hardcoded url

--- src/api/client/axiosInstance.ts
+++ src/api/client/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const TIME_OUT = 20000
 export const axiosInstance = axios.create({
   timeout: TIME_OUT,
-  baseURL: "/api/v1",
+  baseURL: process.env.REACT_APP_URL,
   timeoutErrorMessage: 'The request took too long. Please try again',
   withCredentials: true,
   headers: {
